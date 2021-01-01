fbed81a39270c1895f80d30bd268347b9fe4875d
nahueldev23 <nahuel.dev.23@gmail.com>
2021-01-01 16:14:29 -0300

    blur arreglado con media breakpoints estandar para ratio 16:9

--- app/Http/Controllers/Recortar.php
+++ app/Http/Controllers/Recortar.php
@@ -11,13 +11,10 @@ class Recortar extends Controller
     public function subirImagen(Request $request)
     {
       $mediaQuery = [
-        ['1200','901'],
-        ['1024','769'],
-        ['960','721'],
-        ['768','577'],
-        ['640','481'],
-        ['480','360'],
-        ['320','320'],
+        ['1920','1080'],
+        ['1280','720'],
+        ['854','480'],
+        ['640','360'],
       ];
      
       foreach($request->file('imagen') as $imagen){
@@ -26,7 +23,7 @@ class Recortar extends Controller
           //$nombreOriginal = $request->imagen->getCLientOriginalName(); con extension incluida
           $nombreOriginal = $imagen->getCLientOriginalName();
           $NombreSinExtension = pathinfo($nombreOriginal, PATHINFO_FILENAME);
-          Storage::disk('local')->put('public/'.$mq[0].'/'.$NombreSinExtension.'.webp', $img->save($imagen,100,'webp'));
+          Storage::disk('local')->put('public/'.$mq[1].'/'.$NombreSinExtension.'.webp', $img->save($imagen,100,'webp'));
         }
         
       }
